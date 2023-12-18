12f77902d333daa6152bae8889e2f44e1bf06fc6
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2023-12-18 13:26:19 -0300

    refactor: :hammer: Removido pasta __seedwork duplicada e arquivos não usados

--- src/packages/_seedwork/__init__.py
+++
@@ -1,3 +0,0 @@
-from src._seedwork.command import *
-from src._seedwork.event import *
-from src._seedwork.entity import *
--- src/packages/_seedwork/base_model.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class CustomModel(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
